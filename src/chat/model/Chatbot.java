80469e00dc931f97004ed28a6087ea5ea805c49e
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-10-25 09:46:25 -0600

    Added the length checker

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -76,7 +76,17 @@ public class Chatbot
 	
 	public boolean lengthChecker(String input)
 	{
-		return false;
+		boolean validLength = false;
+		
+		if (input != null)
+		{
+			if (input.length() > 2)
+			{
+				validLength = true;
+			}
+		}
+		
+		return validLength;
 	}
 	
 	public boolean htmlTagChecker(String input)
