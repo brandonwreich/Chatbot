63cd6b2430a9233960e906c41b366bed67c5a95a
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-11-13 09:38:26 -0700

    Take out frame

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,24 +1,6 @@
 package chat.view;
 
-import chat.controller.ChatbotController;
-import javax.swing.JFrame;
-
-public class ChatFrame extends JFrame
+public class ChatFrame
 {
-	private ChatbotController chatController;
-	
-	public ChatFrame(ChatbotController chatController)
-	{
-		super();
-		this.chatController = chatController;
-		
-		setupFrame();
-	}
-	
-	private void setupFrame()
-	{
-		this.setTitle("Chatbot");
-		this.setSize(500,500);
-		this.setVisible(true);
-	}
+
 }
