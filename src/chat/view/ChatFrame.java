fc1c9cca260aab450c3eab701d37bb50b2ba5252
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-11-13 09:35:57 -0700

    Started the frame

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,6 +1,24 @@
 package chat.view;
 
-public class ChatFrame
-{
+import chat.controller.ChatbotController;
+import javax.swing.JFrame;
 
+public class ChatFrame extends JFrame
+{
+	private ChatbotController chatController;
+	
+	public ChatFrame(ChatbotController chatController)
+	{
+		super();
+		this.chatController = chatController;
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setTitle("Chatbot");
+		this.setSize(500,500);
+		this.setVisible(true);
+	}
 }
