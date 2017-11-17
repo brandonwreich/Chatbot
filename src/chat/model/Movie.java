57cd09cb720216e41643536f944bd9785aa3945d
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-11-17 08:55:51 -0700

    Adding documentation

--- src/chat/model/Movie.java
+++ src/chat/model/Movie.java
@@ -12,6 +12,10 @@ public class Movie
 	private LocalDate releaseDate;
 	private double starScore;
 	
+	/**
+	 * Initializes the data members
+	 * @param title
+	 */
 	public Movie(String title)
 	{
 		this.title = "Title: " + title;
@@ -23,76 +27,135 @@ public class Movie
 		this.starScore = 9.99;
 	}
 
+	/**
+	 * Gets the title
+	 * @return title
+	 */
 	public String getTitle()
 	{
 		return title;
 	}
 
+	/**
+	 * Gets the genre
+	 * @return genre
+	 */
 	public String getGenre()
 	{
 		return genre;
 	}
 
+	/**
+	 * Gets the MPAA rating
+	 * @return ratingMPAA
+	 */
 	public String getRatingMPAA()
 	{
 		return ratingMPAA;
 	}
 
+	/**
+	 * Gets the review
+	 * @return review
+	 */
 	public String getReview()
 	{
 		return review;
 	}
 
+	/**
+	 * Gets the movie length
+	 * @return length
+	 */
 	public int getLength()
 	{
 		return length;
 	}
 
+	/**
+	 * Gets the movie release date
+	 * @return releaseDate
+	 */
 	public LocalDate getReleaseDate()
 	{
 		return releaseDate;
 	}
 
+	/**
+	 * Gets the movie star score
+	 * @return starScore
+	 */
 	public double getStarScore()
 	{
 		return starScore;
 	}
 
+	/**
+	 * Sets the movie's title
+	 * @param title
+	 */
 	public void setTitle(String title)
 	{
 		this.title = title;
 	}
 
+	/**
+	 * Sets the movie's genre
+	 * @param genre
+	 */
 	public void setGenre(String genre)
 	{
 		this.genre = genre;
 	}
 
+	/**
+	 * Sets the movie's MPAA rating
+	 * @param ratingMPAA
+	 */
 	public void setRatingMPAA(String ratingMPAA)
 	{
 		this.ratingMPAA = ratingMPAA;
 	}
 
+	/**
+	 * Sets the movie's review
+	 * @param review
+	 */
 	public void setReview(String review)
 	{
 		this.review = review;
 	}
 
+	/**
+	 * Sets the movie's length
+	 * @param length
+	 */
 	public void setLength(int length)
 	{
 		this.length = length;
 	}
 
+	/**
+	 * Sets the movie's release date
+	 * @param releaseDate
+	 */
 	public void setReleaseDate(LocalDate releaseDate)
 	{
 		this.releaseDate = releaseDate;
 	}
 
+	/**
+	 * Sets the movie's star score
+	 * @param starScore
+	 */
 	public void setStarScore(double starScore)
 	{
 		this.starScore = starScore;
 	}
 	
+	/**
+	 * Describes the movies using title, genre, etc.
+	 */
 	public String toString()
 	{
 		String description = "";
