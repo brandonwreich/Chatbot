f71ddce42ad18a5e6736e88c6fdc6f916a6f7fe0
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-11-21 08:38:20 -0700

    Adding the use checkers method

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -2,55 +2,97 @@ package chat.controller;
 
 import chat.model.Chatbot;
 import chat.view.PopupDisplay;
+import chat.view.ChatFrame;
 
 public class ChatbotController
 {
 	private PopupDisplay display;
 	private Chatbot chatbot;
-	
+	private ChatFrame appFrame;
+
 	public ChatbotController()
 	{
 		chatbot = new Chatbot("Brandon Reich");
+
+		// View initialized after Model
 		display = new PopupDisplay();
+		appFrame = new ChatFrame(this);
 	}
+
 	public void start()
 	{
-		String response = display.collectResponse("What do you want to talk about?");
-		
-		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
-		{
-			response = popupChat(response);
-			response = display.collectResponse(response);
-		}
+		display.displayText("Welcome to Chatbot");
 	}
-	
-	private String popupChat(String chat)
+
+	public String interactWithChatbot(String input)
 	{
-		//Assigns a valid value to a variable that will be returned for the method
 		String chatbotSays = "";
-		
-		chatbotSays += chatbot.processConversation(chat);
-		
+
+		if (chatbot.quitChecker(input))
+		{
+			close();
+		}
+
+		chatbotSays += chatbot.processConversation(input);
+
 		return chatbotSays;
 	}
-	
+
 	public String useCheckers(String text)
 	{
 		String response = "";
-		
-		if(chatbot.contentChecker(text))
+
+		if (chatbot.contentChecker(text))
 		{
-			response += "This text matches the special content\n";
+			response += "This text matches the special content";
 		}
-		if(chatbot.cuteAnimalMemeChecker(text))
+
+		if (chatbot.cuteAnimalMemeChecker(text))
 		{
-			response += "";
+			response += "This text matches the cute Animal Memes";
 		}
-		if(chatbot.keyboardMashChecker(sample))
+
+		if (chatbot.htmlTagChecker(text))
 		{
-			response += "";
+			response += "This text matches the html content";
 		}
-		
+
+		if (chatbot.keyboardMashChecker(text))
+		{
+			response += "This text matches the keyboard mash";
+		}
+
+		if (chatbot.movieGenreChecker(text))
+		{
+			response += "This text matches the movie genres";
+		}
+
+		if (chatbot.shoppingListChecker(text))
+		{
+			response += "This text matches the shopping list";
+		}
+
+		if (chatbot.userNameChecker(text))
+		{
+			response += "This text matches the user name content";
+		}
+
 		return response;
+	}
+
+	private void close()
+	{
+		display.displayText("Goodbye");
+		System.exit(0);
+	}
+
+	private String popupChat(String chat)
+	{
+		// Assigns a valid value to a variable that will be returned for the method
+		String chatbotSays = "";
+
+		chatbotSays += chatbot.processConversation(chat);
+
+		return chatbotSays;
 	}
 }
--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,112 @@
 package chat.view;
 
-public class ChatPanel
+import chat.controller.ChatbotController;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import javax.swing.SpringLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChatPanel extends JPanel
 {
+	private ChatbotController appController;
+	private JButton chatButton;
+	private JTextArea chatArea;
+	private JTextField inputField;
+	private SpringLayout appLayout;
+	private JButton checkerButton;
+
+	/**
+	 * Initializes data members and calls methods
+	 * 
+	 * @param appController
+	 */
+	public ChatPanel(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+
+		// Initialize GUI data members
+		chatButton = new JButton("chat");
+		chatArea = new JTextArea(10, 25);
+		inputField = new JTextField(20);
+		appLayout = new SpringLayout();
+		checkerButton = new JButton("Check me");
+		
+
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+
+	/**
+	 * Sets up the panel
+	 */
+	private void setupPanel()
+	{
+		this.setBackground(Color.CYAN);
+		this.setLayout(appLayout);
+		this.add(chatButton);
+		this.add(checkerButton);
+		this.add(chatArea);
+		this.add(inputField);
+		chatArea.setEnabled(false);
+		chatArea.setEditable(false);
+	}
+
+	/**
+	 * Uses SpringLayout to set up the layout of the GUI
+	 */
+
+	private void setupLayout()
+	{
+		appLayout.putConstraint(SpringLayout.NORTH, inputField, 0, SpringLayout.NORTH, chatButton);
+		appLayout.putConstraint(SpringLayout.WEST, inputField, 0, SpringLayout.WEST, chatArea);
+		appLayout.putConstraint(SpringLayout.SOUTH, chatButton, -33, SpringLayout.SOUTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, chatButton, 0, SpringLayout.EAST, chatArea);
+		appLayout.putConstraint(SpringLayout.NORTH, chatArea, 20, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.WEST, chatArea, 25, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.EAST, chatArea, -25, SpringLayout.EAST, this);
+		appLayout.putConstraint(SpringLayout.WEST, checkerButton, 0, SpringLayout.WEST, inputField);
+		appLayout.putConstraint(SpringLayout.SOUTH, checkerButton, 30, SpringLayout.SOUTH, inputField);
+
+	}
+
+	/**
+	 * Links the buttons and stuff to their appropriate listeners
+	 */
+
+	private void setupListeners()
+	{
+		chatButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				String userText = inputField.getText();
+				String displayText = appController.interactWithChatbot(userText);
+				chatArea.append(displayText);
+				inputField.setText("");
+
+			}
+		});
+		
+		checkerButton.addActionListener(new ActionListener()
+				{
+				public void ationPerformed(ActionEvent click)
+				{
+					String userText = inputField.getText();
+					String displayText = appController.useCheckers(userText);
+					chatArea.append(displayText);;
+					inputField.setText("");
+				}
+				});
+	}
 
 }
