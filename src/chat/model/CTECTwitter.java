d0ba22d6807d3a4d6fdabe9444b34b38186e03ae
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-03-06 09:00:32 -0700

    Created a method to remove boring words from the list

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -59,6 +59,21 @@ public class CTECTwitter
 		return mostCommon;
 	}
 
+	private void trimTheBoringWords(String [] boringWords)
+	{
+		for (int index = tweetedWords.size() - 1; index >= 0; index --)
+		{
+			for (int boringIndex = 0; boringIndex < boringWords.length; boringIndex++)
+			{
+				if (tweetedWords.get(index).equals(boringWords[boringIndex]))
+				{
+					tweetedWords.remove(index);
+					boringIndex = Integer.MAX_VALUE;
+				}
+			}
+		}
+	}
+	
 	private void collectTweets(String username)
 	{
 		searchedTweets.clear();
